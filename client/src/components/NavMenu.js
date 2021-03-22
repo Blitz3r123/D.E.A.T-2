33392ee21dc7dc8626a88fbba1db60f5fd1e001d
Kaleem Peeroo <kaleem@Kaleems-MacBook-Pro.local>
2021-03-22 19:35:42 +0000

    Added navmenu

--- client/src/components/NavMenu.js
+++ client/src/components/NavMenu.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { HomeOutline, CreateOutline, StatsChartOutline } from 'react-ionicons';
+import { Link } from 'react-router-dom';
 
 export default class NavMenu extends Component{
     render(){
         return(
             <ListGroup>
-                <ListGroup.Item>Home</ListGroup.Item>
-                <ListGroup.Item>Create</ListGroup.Item>
-                <ListGroup.Item>Analyse</ListGroup.Item>
+                <ListGroup.Item>
+                    <Link to="/" className="navmenu-link">
+                        <HomeOutline/>
+                        <span style={{ marginLeft: 10 }}>Home</span>
+                    </Link>
+                </ListGroup.Item>
+                
+                <ListGroup.Item>
+                    <Link to="/create" className="navmenu-link">
+                        <CreateOutline/>
+                        <span style={{ marginLeft: 10 }}>Create</span>
+                    </Link>
+                </ListGroup.Item>
+                
+                <ListGroup.Item>
+                    <Link to="/analyse" className="navmenu-link">
+                        <StatsChartOutline/>
+                        <span style={{ marginLeft: 10 }}>Analyse</span>
+                    </Link>
+                </ListGroup.Item>
             </ListGroup>
         );
     }
