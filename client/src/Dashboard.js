fcf311c03828eb44a6aa0ed1c939dfd694e422f6
Kaleem Peeroo <kaleem@Kaleems-MacBook-Pro.local>
2021-03-22 18:51:22 +0000

    Added dashboard.js

---
+++ client/src/Dashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Dashboard extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>dashboard</p>
+            </div>
+        );
+    }
+}
