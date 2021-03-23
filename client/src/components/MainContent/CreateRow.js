00947312c171f38d622ed2de238f899bec30f92b
Kaleem Peeroo <kaleem@Kaleems-MacBook-Pro.local>
2021-03-23 18:00:27 +0000

    Added icons to create buttons on dboard

--- client/src/components/MainContent/CreateRow.js
+++ client/src/components/MainContent/CreateRow.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { CloudUploadOutline, CloudDownloadOutline, ConstructOutline } from 'react-ionicons';
 
 export default class CreateRow extends Component{
     render(){
@@ -13,15 +14,18 @@ export default class CreateRow extends Component{
 
                 <div className="create-buttons-container">
 
-                    <Button variant="outline-primary">
+                    <Button variant="outline-primary" className="create-button">
+                        <CloudUploadOutline color="#007bff"/>
                         <Link to="/create/publisher">Publisher</Link>
                     </Button>
 
-                    <Button variant="outline-primary">
+                    <Button variant="outline-primary" className="create-button">
+                        <CloudDownloadOutline color="#007bff"/>
                         <Link to="/create/subscriber">Subscriber</Link>
                     </Button>
 
-                    <Button variant="outline-primary">
+                    <Button variant="outline-primary" className="create-button">
+                        <ConstructOutline color="#007bff"/>
                         <Link to="/create/test">Test</Link>
                     </Button>
 
