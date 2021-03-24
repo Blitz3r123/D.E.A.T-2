e72e85eb31cca1f4a7cbd1376432de1ea885690d
Kaleem Peeroo <kaleem@Kaleems-MacBook-Pro.local>
2021-03-24 13:47:13 +0000

    Added empty message for existing row in create

--- client/src/components/CreateContent/ExistingRow.js
+++ client/src/components/CreateContent/ExistingRow.js
@@ -1,11 +1,30 @@
 import react, { Component } from 'react';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import './../../css/CreateContent/ExistingRow.css';
+import emptyImage from './../../assets/EmptyExistingRowImage2.jpg';
 
 export default class ExistingRow extends Component{
     render(){
         return(
-            <div>
-                <p>Existing row here</p>
-            </div>
+            <Card>
+                <Card.Header style={{fontWeight: 'bold'}}>Existing Participants/Tests</Card.Header>
+                <Card.Body>
+
+                    <div className="existing-row-empty-message-container">
+                        <div className="empty-message-container">
+                            <img className="existing-row-empty-message-image" src={emptyImage}></img>
+                            <p>
+                                You don't seem to have any existing tests/participants...Why not create 
+                                a <Link className="empty-link" to="/create/publisher">publisher</Link>, <Link className="empty-link" to="/create/subscriber">subscriber</Link>, or
+                                a <Link className="empty-link" to="/create/test">test</Link>?
+                            </p>
+                        </div>
+                    </div>
+
+                </Card.Body>
+            </Card>
         );
     }
 }
