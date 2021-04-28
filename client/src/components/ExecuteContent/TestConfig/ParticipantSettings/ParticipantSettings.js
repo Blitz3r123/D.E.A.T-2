d5aafe0bd57aa1ac5448e9e4a3313d204ae14f19
Kaleem Peeroo <kaleem@Kaleems-MacBook-Pro.local>
2021-04-28 21:03:19 +0100

    Removed main row

--- client/src/components/ExecuteContent/TestConfig/ParticipantSettings/ParticipantSettings.js
+++ client/src/components/ExecuteContent/TestConfig/ParticipantSettings/ParticipantSettings.js
@@ -7,7 +7,7 @@ export default class ParticipantSettings extends Component{
             <Card style={{maxHeight: '76.5vh'}}>
                 <Card.Header>Pub One</Card.Header>
                 <div style={{overflowY: 'scroll'}}>
-                    
+                    participant settings here
                 </div>
             </Card>
         );
